b154be45914074a16bd068a383edddfccd0b02b1
belema7 <belemagirma31@gmail.com>
2025-11-26 04:50:47 -0800

    add routing setup to project

--- src/routing/Routing.jsx
+++ src/routing/Routing.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
+import About from '../pages/About'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import Dashboard from '../pages/Dashboard'
+import MedicineInput from '../pages/MedicineInput'
+import NotFound from '../pages/NotFound'
 const Routing = () => {
   return (
     <Routes>
         <Route path='/'element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/medicineinput' element={<MedicineInput/>}/>
+        <Route path='*' element={<NotFound/>}/>
     </Routes>
-  )
+  )   
 }
 
 export default Routing
